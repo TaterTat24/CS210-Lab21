5264678c700ab05332de92c69401a8d4fa018eea
TaterTat24 <C24Benjamin.Tat@afacademy.af.edu>
2022-10-18 13:35:24 -0600

    Lab 21 Solved

--- lab21-1.c
+++ lab21-1.c
@@ -1,7 +1,7 @@
 /** lab21-1.c
  * ===========================================================
- * Name: First Last
- * Section: xxx
+ * Name: Benjamin Tat
+ * Section: T6
  * Project: Lab 21
  * ===========================================================
  */
@@ -10,7 +10,39 @@
 #include <stdlib.h>
 
 int main() {
+    FILE* inputFile = NULL; // File pointer,
+    char fileName[100];
+    char line[1000];
+    //int i;
+    //int commacount = 0;
 
+    scanf("%s", fileName);
+
+    inputFile = fopen(fileName, "r");
+
+    if (inputFile == NULL) {
+        printf("Could not open file.\n");
+        return -1; // -1 indicates error
+    }
+
+    while (!feof(inputFile)) {
+        //commacount = 0;
+        fscanf(inputFile, "%*d,%[^,],%*[^\n]\n", line);
+        printf("%s", line);
+
+        /*
+        fgets(line, 1000, inputFile);
+        for (i = 0; i < strlen(line); i++) {
+            if (line[i] == ',') {
+                commacount = commacount + 1;
+            }
+            if (commacount == 1 && line[i] != ','){
+                printf("%c", line[i]);
+            }
+        }
+        */
+
+        printf("\n");
+    }
     return 0;
-
 }
--- lab21-2.c
+++ lab21-2.c
@@ -1,7 +1,7 @@
 /** lab21-2.c
  * ===========================================================
- * Name: First Last
- * Section: xxx
+ * Name: Benjamin Tat
+ * Section: T6
  * Project: Lab 21
  * ===========================================================
  */
@@ -10,7 +10,45 @@
 #include <stdlib.h>
 
 int main() {
+    FILE* inputFile = NULL; // File pointer,
+    char fileName[100];
 
-    return 0;
+    char time[100];
+    char title[100];
+    char previousTitle[100] = "dog";
+    char rating[100];
+    int i = 0;
+
+    scanf("%s", fileName);
+
+    inputFile = fopen(fileName, "r");
+
+    if (inputFile == NULL) {
+        printf("Could not open file.\n");
+        return -1; // -1 indicates error
+    }
+
+    while (!feof(inputFile)) {
+        fscanf(inputFile, "%[^,],%[^,],%[^\n]\n", time, title, rating);
+        title[44] = '\0';
+
+        if (strcmp(title, previousTitle) != 0){
+            if (i != 0){
+                printf("\n");
+                printf("%-44s | %5s | %s", title, rating, time);
+                i = 0;
+            }
+            else {
+                printf("\n");
+                printf("%-44s | %5s | %s", title, rating, time);
+            }
+        }
+        else {
+            i = i + 1;
+            printf(" %s", time);
+        }
+        strcpy(previousTitle, title);
+    }
     
+    return 0;   
 }
